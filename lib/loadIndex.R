670c8bb7baa67bb81f6c288e956041439380ee04
Luis P <lprisan@hotmail.com>
2015-04-07 17:39:10 +0200

    added session load difference part. Done!

--- lib/loadIndex.R
+++ lib/loadIndex.R
@@ -20,3 +20,49 @@ calculateLoadIndexSession <- function(data, meanlabel="value.pupilMean", sdlabel
     
     data
 }
+
+
+# This function gets a data frame with eye metrics for 10s windows
+# in a session, normalizes each session data with that of the first episode
+# in the session, and calculates the Load Index for each window in that session,
+# using the median, not of that session, but of a "session of reference".
+# Returns the same dataset with added columns
+# Since in some cases the field labels may vary, they can also be specified explicitly
+calculateLoadIndexOtherSessionNormalized <- function(data, refsession, meanlabel="value.pupilMean", sdlabel="value.pupilSD", fixlabel="value.longFix", saclabel="value.sacSpd"){
+
+    # We normalize the data for each session with that of the first episode
+    normdata <- data.frame()
+    for(session in sessions){
+
+        sessdata <- data[data$Session == session,]
+        
+        normsessdata <- sessdata
+        normsessdata[,meanlabel] <- sessdata[,meanlabel]/sessdata[1,meanlabel]
+        normsessdata[,sdlabel] <- sessdata[,sdlabel]/sessdata[1,sdlabel]
+        normsessdata[,fixlabel] <- sessdata[,fixlabel]/sessdata[1,fixlabel]
+        normsessdata[,saclabel] <- sessdata[,saclabel]/sessdata[1,saclabel]
+        
+        # We join the new data into a dataset with all sessions normalized data
+        if(length(normdata)==0) normdata <- normsessdata
+        else normdata <- rbind(normdata,normsessdata)
+    }  
+    
+    # We calculate the session of reference's (normalized) medians
+    refdata <- normdata[normdata$Session == refsession,]
+    meansessionmed <- median(refdata[,meanlabel], na.rm=T)
+    sdsessionmed <- median(refdata[,sdlabel], na.rm=T)
+    longsessionmed <- median(refdata[,fixlabel], na.rm=T)
+    sacsessionmed <- median(refdata[,saclabel], na.rm=T)
+    
+    
+    # We calculate the load indices using the reference medians
+    normdata$Above.Mean <- as.numeric(normdata[,meanlabel] > meansessionmed)
+    normdata$Above.SD <- as.numeric(normdata[,sdlabel] > sdsessionmed)
+    normdata$Above.Fix <- as.numeric(normdata[,fixlabel] > longsessionmed)
+    normdata$Above.Sac <- as.numeric(normdata[,saclabel] > sacsessionmed)
+    # We calculate the Load Index simply summing the different median cuts
+    normdata$Load <- normdata$Above.Mean + normdata$Above.SD + normdata$Above.Fix + normdata$Above.Sac
+    
+    normdata
+}
+
